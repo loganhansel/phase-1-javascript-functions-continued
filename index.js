328e8916b852de958e22021ef1baea627581559a
loganhansel <loganhansel23@gmail.com>
2022-04-27 16:19:15 -0400

    commit tree 2a9dce72915d67b85b4f5aa67154a7097d8d85c9

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    const innerFunction = function (something = "special") {
+        return `You are ${flair}${something}${flair}!`
+    }
+    return innerFunction;
+}
